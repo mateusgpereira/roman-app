5b85038d4ce38f48babca45a28066b84b39f4b88
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2020-11-24 17:45:38 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import routes from './routes'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(routes)
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`)
+})
